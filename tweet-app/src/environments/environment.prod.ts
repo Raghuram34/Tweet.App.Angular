13760eda1d54fc57b473de18f6fcf1b3aaa08fe2
Raghuram34 <raghuramreddyipl@gmail.com>
2022-07-04 08:57:56 +0530

    updated yml

--- tweet-app/src/environments/environment.prod.ts
+++ tweet-app/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  UserAPIPath : "https://tweetauth.azurewebsites.net/api/user",
-  TweetAPIPath: "https://tweetcore.azurewebsites.net/api/tweet"
+  UserAPIPath : "https://tweetcore.azurewebsites.net/api/user",
+  TweetAPIPath: "https://tweetauth.azurewebsites.net/api/tweet"
 };
