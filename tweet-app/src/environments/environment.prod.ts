4cd1f6a84d707b079c90a271ab0b12bd8d78abfe
Raghuram34 <raghuramreddyipl@gmail.com>
2022-07-04 08:40:55 +0530

    update env file

--- tweet-app/src/environments/environment.prod.ts
+++ tweet-app/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  UserAPIPath : "https://localhost:44317/api/user",
-  TweetAPIPath: "https://localhost:44351/api/tweet"
+  UserAPIPath : "https://tweetauth.azurewebsites.net/api/user",
+  TweetAPIPath: "https://tweetcore.azurewebsites.net/api/tweet"
 };
